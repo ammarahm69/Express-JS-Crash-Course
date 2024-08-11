8ef5bf8d76fa34b7484f643b45720b7acc891a38
Ammar Ahmed <ammar69aa59@gmail.com>
2024-08-11 05:19:26 -0700

    hello world

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 
+app.get('/',(req,res) => {
+    res.send({message:'Hello World'});
+}) ;
+
+
+
+
+
+//server port listen
 app.listen(8000, () => {
   console.log(`Server running on port 8000`);
 });
